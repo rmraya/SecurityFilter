8d2be09a4c623b6ac02c691f2cdb0a05e84798e2
Rodolfo M. Raya <rmraya@maxprograms.com>
2023-12-13 07:22:22 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# SecurityFilter
+
+Custom security filter for Tomcat 9 to be used when serving static content
+
+## Installation
+
+1. Clone this repository
+2. Copy SecurityFilter.jar to Tomcat's `/lib` folder
+3. Edit Tomcat's `conf/web.xml` and add the following lines in Filterss section:
+
+```xml
+  <filter>
+    <filter-name>SecurityFilter</filter-name>
+    <filter-class>com.maxprograms.filter.SecurityFilter</filter-class>
+  </filter>
+
+  <filter-mapping>
+    <filter-name>SecurityFilter</filter-name>
+    <url-pattern>/*</url-pattern>
+    <dispatcher>REQUEST</dispatcher>
+  </filter-mapping>
+```
---
+++ src/com/maxprograms/filter/SecurityFilter.java
@@ -0,0 +1,65 @@
+/*******************************************************************************
+ * Copyright (c) 2023 Maxprograms.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 1.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/org/documents/epl-v10.html
+ *
+ * Contributors:
+ *     Maxprograms - initial API and implementation
+ *******************************************************************************/
+package com.maxprograms.filter;
+
+import java.io.IOException;
+import java.lang.System.Logger;
+import java.lang.System.Logger.Level;
+import java.nio.charset.StandardCharsets;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+public class SecurityFilter implements Filter {
+
+    private static Logger logger = System.getLogger(SecurityFilter.class.getName());
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletResponse res = (HttpServletResponse) response;
+
+        res.addHeader("X-Frame-Options", "SAMEORIGIN");
+        res.addHeader("X-XSS-Protection", "1; mode=block");
+        res.addHeader("X-Content-Type-Options", "nosniff");
+        res.addHeader("Cache-Control", "no-cache");
+        res.addHeader("Pragma", "no-cache");
+        res.addHeader("Expires", "0");
+        res.addHeader("Strict-Transport-Security", "max-age=31536000; includeSubDomains");
+        res.addHeader("X-Permitted-Cross-Domain-Policies", "master-only");
+        res.addHeader("Content-Security-Policy", "report-uri https://dev.maxprograms.com");
+        res.addHeader("Referrer-Policy", "no-referrer-when-downgrade");
+        res.addHeader("Permissions-Policy", "microphone=(), camera=()");
+
+        res.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        try {
+            chain.doFilter(request, response);
+        } catch (IOException e) {
+            logger.log(Level.ERROR, e);
+        }
+    }
+
+    @Override
+    public void destroy() {
+        // do nothing
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+        // do nothing
+    }
+}
